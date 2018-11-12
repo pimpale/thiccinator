cb7886674b8b4d121fcb15046e614a27d585abc8
Christopher Dumas <christopherdumas@gmail.com>
2018-11-12 12:42:13 -0800

    More general with rules, and uses iterators

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,56 @@
+
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
 use std::io::{self, BufRead};
+use std::io::Read;
+
 fn main() {
-	let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        println!("{}", thiccinate(line.unwrap()));
+    let stdin = io::stdin();
+
+    // default rules
+    let mut rules: HashMap<String, String> = [
+        ("a", "卂"),
+        ("b", "乃"),
+        ("c", "匚"),
+        ("d", "刀"),
+        ("e", "乇"),
+        ("f", "下"),
+        ("g", "厶"),
+        ("h", "卄"),
+        ("i", "工"),
+        ("j", "丁"),
+        ("k", "长"),
+        ("l", "乚"),
+        ("m", "从"),
+        ("n", "𠘨"),
+        ("o", "口"),
+        ("p", "尸"),
+        ("q", "㔿"),
+        ("r", "尺"),
+        ("s", "丂"),
+        ("t", "丅"),
+        ("u", "凵"),
+        ("v", "リ"),
+        ("w", "山"),
+        ("x", "乂"),
+        ("y", "丫"),
+        ("z", "乙"),
+    ].iter()
+        .cloned()
+        .map(|(a, b)| (a.to_string(), b.to_string()))
+        .collect();
+	
+    for nl in stdin.lock().lines().filter_map(|l| {
+        l.map(|l: String| rule_replace(l.to_string(), &rules)).ok()
+    })
+    {
+        println!("{}", nl);
     }
 }
-	
 
-fn thiccinate(text:String) -> String {
-	let mut thiccinated = String::new();
-	for c in text.to_lowercase().chars() {
-		thiccinated.push(match c {
-		'a' => '卂',
-		'b' => '乃',
-		'c' => '匚',
-		'd' => '刀',
-		'e' => '乇',
-		'f' => '下',
-		'g' => '厶',
-		'h' => '卄',
-		'i' => '工',
-		'j' => '丁',
-		'k' => '长',
-		'l' => '乚',
-		'm' => '从',
-		'n' => '𠘨',
-		'o' => '口',
-		'p' => '尸',
-		'q' => '㔿',
-		'r' => '尺',
-		's' => '丂',
-		't' => '丅',
-		'u' => '凵',
-		'v' => 'リ',
-		'w' => '山',
-		'x' => '乂',
-		'y' => '丫',
-		'z' => '乙',
-		_ => c
-		});
-	}
-	thiccinated
+fn rule_replace(text: String, hash: &HashMap<String, String>) -> String {
+    hash.iter().fold(text.to_lowercase(), |txt, subst| {
+        str::replace(txt.as_str(), subst.0, subst.1)
+    })
 }
